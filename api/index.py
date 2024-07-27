1ef901bd596d427f60233a3690b6956421e3c78f
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2024-07-27 13:31:34 +0700

    fix: udf crud

--- api/index.py
+++ api/index.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
+from typing import List
+from models import PyObjectId, UDF, Configs
+from bson import ObjectId
 
 load_dotenv()
 
@@ -20,34 +23,123 @@ app.add_middleware(
 conn_string = os.getenv("CONN_STRING")
 client = MongoClient(conn_string)
 db = client["iperfect-db"]
-collection = db["function-code"]
+udf_collection = db["function-code"]
+config_collection = db["config"]
 
 
-class Item(BaseModel):
-    code: str
+@app.post("/udf/", response_model=UDF)
+async def create_udf(udf: UDF):
+    udf_dict = udf.model_dump(by_alias=True)
+    result = udf_collection.insert_one(udf_dict)
+    udf_dict["_id"] = result.inserted_id
+    return udf_dict
 
 
-def serialize_dict(document):
-    document["_id"] = str(document["_id"])
-    return document
+@app.get("/udf/{udf_id}", response_model=UDF)
+async def get_udf(udf_id: str):
+    if (udf := udf_collection.find_one({"_id": ObjectId(udf_id)})) is not None:
+        return udf
+    raise HTTPException(status_code=404, detail="UDF not found")
+
+@app.get("/udf/", response_model=List[UDF])
+async def get_all_udfs():
+    udfs = []
+    for udf in udf_collection.find():
+        udf["_id"] = str(udf["_id"])
+        udfs.append(udf)
+    return udfs
+
+@app.post("/config/", response_model=Configs)
+async def create_config(config: Configs):
+    config_dict = config.model_dump(by_alias=True)
+    if not all (udf_collection.find_one({"_id": pid}) for pid in config.udf_ids):
+        raise HTTPException(status_code=404, detail="One or more UDF not found")
+    result = config_collection.insert_one(config_dict)
+    config_dict["_id"] = result.inserted_id
+    return config_dict
 
 
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to the Iperfect API"}
+@app.get("/config/{config_id}", response_model=Configs)
+async def get_config(config_id: str):
+    if (config := config_collection.find_one({"_id": ObjectId(config_id)})) is not None:
+        config["_id"] = str(config["_id"])
+        config["udf_ids"] = [str(pid) for pid in config["udf_ids"]]
+        return config
+    raise HTTPException(status_code=404, detail="Config not found")
+
+@app.get("/config/", response_model=List[Configs])
+async def get_all_configs():
+    configs = []
+    for config in config_collection.find():
+        config["_id"] = str(config["_id"])
+        config["udf_ids"] = [str(pid) for pid in config["udf_ids"]]
+        configs.append(config)
+    return configs
+
+@app.get("/config/{config_id}/details")
+async def get_config_details(config_id: str):
+    if (config := config_collection.find_one({"_id": ObjectId(config_id)})) is not None:
+        udfs = list(udf_collection.find({"_id": {"$in": config["udf_ids"]}}))
+        for udf in udfs:
+            udf["_id"] = str(udf["_id"])
+        config["udfs"] = udfs
+        config["_id"] = str(config["_id"])
+        config["udf_ids"] = [str(pid) for pid in config["udf_ids"]]
+        return config
+    raise HTTPException(status_code=404, detail="Config not found")
+
+@app.post("/udf/{config_id}/add", response_model=Configs)
+async def create_udf_and_add_to_config(config_id: str, udf: UDF):
+    udf_dict = udf.model_dump(by_alias=True)
+    result = udf_collection.insert_one(udf_dict)
+    udf_dict["_id"] = result.inserted_id
+    new_udf = UDF(**udf_dict)
+
+    if (config := config_collection.find_one({"_id": ObjectId(config_id)})) is not None:
+        config_collection.update_one(
+            {"_id": ObjectId(config_id)},
+            {"$push": {"udf_ids": new_udf.id}}
+        )
+        config["udf_ids"].append(new_udf.id)
+        config["_id"] = str(config["_id"])
+        config["udf_ids"] = [str(pid) for pid in config["udf_ids"]]
+        return config
+
+    raise HTTPException(status_code=404, detail="config not found")
+# class Formula(BaseModel):
+#     name: str
+#     function: UDF
+
+# class Group(BaseModel):
+#     name: str
+#     formulas: List[Formula]
+
+# class Config(BaseModel):
+#     name: str
+#     groups: List[Group]
+
+# def serialize_dict(document):
+#     document["_id"] = str(document["_id"])
+#     return document
 
 
-@app.post("/code/")
-async def create_code(item: Item):
-    code = item.model_dump()
-    collection.insert_one(code)
-    return [serialize_dict(code)]
+# @app.get("/")
+# async def read_root():
+#     return {"message": "Welcome to the Iperfect API"}
 
 
-@app.get("/code/")
-async def read_code():
-    code = list(collection.find())
-    return [serialize_dict(item) for item in code]
+# @app.post("/code/")
+# async def create_code(item: UDF):
+#     code = item.model_dump()
+#     collection.insert_one(code)
+#     return [serialize_dict(code)]
+
+
+# @app.get("/code/")
+# async def read_code():
+#     code = list(collection.find())
+#     return [serialize_dict(item) for item in code]
+
 
 def main(request):
     return app(request)
---
+++ api/models.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional, Any
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v: Any, field) -> ObjectId:
+        if isinstance(v, ObjectId):
+            return v
+        if isinstance(v, str) and ObjectId.is_valid(v):
+            return ObjectId(v)
+        raise ValueError('Invalid objectid')
+
+    @classmethod
+    def __get_pydantic_json_schema__(cls, field_schema: dict[str, Any]) -> None:
+        field_schema.update(type='string')
+
+class Input(BaseModel):
+    name: str
+    value: str
+    default_value: int
+
+class UDF(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str
+    output_type: str
+    inputs: List[Input]
+    code: str
+
+    class Config:
+        populate_by_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
+
+class Configs(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str
+    udf_ids: List[PyObjectId] = Field(default_factory=list)
+
+    class Config:
+        populate_by_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
